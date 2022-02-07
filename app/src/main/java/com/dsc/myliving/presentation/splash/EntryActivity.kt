b6e93d307fcbc71b0f0bf5e4f03a69261408d464
linusmuema <linus.m.muema@gmail.com>
2022-02-07 19:31:22 +0300

    - added dark theme support

---
+++ app/src/main/java/com/dsc/myliving/presentation/splash/EntryActivity.kt
@@ -0,0 +1,11 @@
+package com.dsc.myliving.presentation.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class EntryActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
