e2a4102fa78ac68f66beddebb6f7aa94cf95f705
linusmuema <linus.m.muema@gmail.com>
2022-02-07 20:14:00 +0300

    - added the adaptive icon in splash screen

--- app/src/main/java/com/dsc/myliving/presentation/splash/EntryActivity.kt
+++ app/src/main/java/com/dsc/myliving/presentation/splash/EntryActivity.kt
@@ -3,9 +3,18 @@ package com.dsc.myliving.presentation.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.dsc.myliving.presentation.theme.MyLivingTheme
 
 class EntryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+            MyLivingTheme {
+
+            }
+        }
     }
 }
