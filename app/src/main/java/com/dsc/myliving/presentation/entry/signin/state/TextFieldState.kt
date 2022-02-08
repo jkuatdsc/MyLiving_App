7e197754bd1e683bd8276a0302262441cb089861
Otieno Samwel <otienosamwel135@gmail.com>
2022-02-08 15:20:01 +0300

    create initial structure and state class

---
+++ app/src/main/java/com/dsc/myliving/presentation/entry/signin/state/TextFieldState.kt
@@ -0,0 +1,17 @@
+package com.dsc.myliving.presentation.entry.signin.state
+
+import androidx.compose.runtime.*
+
+class TextFieldState(val label: String) {
+    var value: String by mutableStateOf("")
+    val onValueChange: (t: String) -> Unit = { value = it }
+
+    fun validate() {
+
+    }
+}
+
+@Composable
+fun rememberTextFieldState(label: String) = remember {
+    TextFieldState(label = label)
+}
