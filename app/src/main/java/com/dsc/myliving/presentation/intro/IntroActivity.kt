4262739ae90b90334dfeef6a2ce0620b6c558a84
linusmuema <linus.m.muema@gmail.com>
2022-02-09 11:44:07 +0300

    - initialise the DI

---
+++ app/src/main/java/com/dsc/myliving/presentation/intro/IntroActivity.kt
@@ -0,0 +1,11 @@
+package com.dsc.myliving.presentation.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class IntroActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
